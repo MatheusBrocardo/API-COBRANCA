de287340f23fe47f18eba3bd3f69ad7cd287be43
Matheus Brocardo <matheus1brocardo@gmail.com>
2024-12-18 11:34:55 -0300

    Update init app

---
+++ db/settings.py
@@ -0,0 +1,4 @@
+import cx_Oracle
+cx_Oracle.init_oracle_client(lib_dir=r"C:\oracle\instantclient_21_3")
+SringConnect = f"{os.getenv('OracleUser')}/{os.getenv('OraclePass')}@{os.getenv('OracleAdress')}/{os.getenv('OracleOwner')}"
+conn = cx_Oracle.connect(SringConnect)
